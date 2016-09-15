8796d10cad666eb4ad23d4a56517375c7e535546
projectFirehive <projectFirehive@gmail.com>
2016-09-15 17:28:15 +0100

    Base Interpreter
    Added the base interpreter, can handle all functions other than loops
    and I/O.

---
+++ brainfuckInterpreter.py
@@ -0,0 +1,31 @@
+x = 0
+y = [0]*30000
+z = 0
+brainfuck = input()
+brainfuck.split
+print (brainfuck)
+for i in brainfuck:
+    x+= 1
+    if brainfuck[x] = (">"):
+        z+= 1
+    elif brainfuck[x] = ("<"):
+        z-= 1
+    elif brainfuck[x] = ("+"):
+        y[z] += 1
+    elif brainfuck[x] = ("-"):
+        y[z] -= 1
+    elif brainfuck[x] = ("["):
+        if y[z] = 0:
+            #function to skip to closing bracket
+    elif brainfuck[x] = ("]"):
+        if y[z] != 0:
+            #function to skip to opening bracket
+    elif brainfuck[x] = (","):
+        cell = input("Input a value: ")
+        #function to convert value to ASCII
+        y[z] = cell
+    elif brainfuck[x] = ("."):
+        output = #function to convert ASCII to value
+        print (output)
+    else:
+        continue
